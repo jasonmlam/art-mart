12b25c1ecb0ade21785a8721d244de18c3262ea1
jasonmlam <jasonlam@knights.ucf.edu>
2022-02-19 22:02:56 -0500

    Content added to all pages

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get("/", function (req, res){
+app.get("/home", function (req, res){
     res.sendFile(__dirname + "/index.html");
 })
 
