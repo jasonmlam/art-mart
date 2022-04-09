4a3bae03f6bbbea5707c159ffad6f36e5fc27478
jasonmlam <jasonlam@knights.ucf.edu>
2022-04-08 23:41:34 -0400

    changed javscript form

--- javascript/form.js
+++ javascript/form.js
@@ -12,30 +12,69 @@ function checkForm() {
     var numbers = /^[0-9]+$/; /* used to compare numbers */
     var emailformat = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/; /* used to compare valid email address */    
 
-    /* check fields are empty */
-    if (firstName==""||lastName=="",address==""||city=="",state==""||zip=="",ptype==""||email=="") {
-        alert("Please Fill All Required Field");
-    return false; 
+    if (document.userForm.firstName.value == "")
+    {
+        alret("Please provide your first name");
+        document.userForm.firstName.focus();
+        return false;
     }
 
-    /* check length of input data if its 1 it will show alert msg */
-    if (firstName.length<=1||lastName.length<=1,address.length<=1||city.length<=1,zip.length<=1||email.length<=1) {
-        alert("input must be greater than one character");      
+    if (document.userForm.lastName.value == "")
+    {
+        alret("Please provide your last name");
+        document.userForm.firstName.focus();
+        return false;
     }
 
-    /* check zip code must be numbers and length should not be less than or greater than 5 */
-    if ((!zip.match(numbers)) || zip.length!==5){
-        alert('enter only numbers or length of zip code must be equal to 5');
+    if (document.userForm.address.value == "")
+    {
+        alret("Please provide your street address");
+        document.userForm.address.focus();
+        return false;
     }
 
-    /* check phone number must be numbers and length should not be less than or greater than 10 */
-    if ((!phone.match(numbers)) || phone.length!==10){
-        alert('enter only numbers or length of Phone number must be equal to 10');
-    }
+    if (document.userForm.city.value == "")
+    {
+        alret("Please provide your city");
+        document.userForm.city.focus();
+        return false;
+    }    
 
-    /* check valid email id */
-    if ((!email.match(emailformat))){
-        alert('please enter valid email id');
-    }
+    if ( document.myForm.Zip.value == "" || 
+        isNaN( document.myForm.Zip.value ) ||
+        document.myForm.Zip.value.length != 5 ) 
+    {
+        alert( "Please provide a zip in the format #####." );
+        document.myForm.Zip.focus() ;
+        return false;
+    }      
+
+    // /* check fields are empty */
+    // if (firstName==""||lastName=="",address==""||city=="",state==""||zip=="",ptype==""||email=="") {
+    //     alert("Please Fill All Required Field");
+    // return false; 
+    // }
+
+    // /* check length of input data if its 1 it will show alert msg */
+    // if (firstName.length<=1||lastName.length<=1,address.length<=1||city.length<=1,zip.length<=1||email.length<=1) {
+    //     alert("input must be greater than one character");      
+    // }
+
+    // /* check zip code must be numbers and length should not be less than or greater than 5 */
+    // if ((!zip.match(numbers)) || zip.length!==5){
+    //     alert('enter only numbers or length of zip code must be equal to 5');
+    // }
+
+    // /* check phone number must be numbers and length should not be less than or greater than 10 */
+    // if ((!phone.match(numbers)) || phone.length!==10){
+    //     alert('enter only numbers or length of Phone number must be equal to 10');
+    // }
+
+    // /* check valid email id */
+    // if ((!email.match(emailformat))){
+    //     alert('please enter valid email id');
+    // }
+
+    
 
 }
