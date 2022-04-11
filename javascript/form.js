0658278f0d9d21e97dc33fec49c00abd000dbfd8
jasonmlam <jasonlam@knights.ucf.edu>
2022-04-10 22:29:47 -0400

    alert

--- javascript/form.js
+++ javascript/form.js
@@ -12,50 +12,51 @@ function checkForm() {
     // var numbers = /^[0-9]+$/; /* used to compare numbers */
     // var emailformat = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/; /* used to compare valid email address */    
 
-    let form = document.getElementById(form);
+    let form = document.getElementById("form");
     let data = new FormData(form);
-
-    alert(data);
+    console.log(data.get("zip"))
+    return false;
+    
 
     // console.log(formObject);                                             
 
-    if (document.userForm.firstName.value == "")
-    {
-        alert("Please provide your first name");
-        document.userForm.firstName.focus();
-        return false;
-    }
+    // if (document.userForm.firstName.value == "")
+    // {
+    //     alert("Please provide your first name");
+    //     document.userForm.firstName.focus();
+    //     return false;
+    // }
 
-    if (document.userForm.lastName.value == "")
-    {
-        alert("Please provide your last name");
-        document.userForm.firstName.focus();
-        return false;
-    }
+    // if (document.userForm.lastName.value == "")
+    // {
+    //     alert("Please provide your last name");
+    //     document.userForm.firstName.focus();
+    //     return false;
+    // }
 
-    if (document.userForm.address.value == "")
-    {
-        alert("Please provide your street address");
-        document.userForm.address.focus();
-        return false;
-    }
+    // if (document.userForm.address.value == "")
+    // {
+    //     alert("Please provide your street address");
+    //     document.userForm.address.focus();
+    //     return false;
+    // }
 
-    if (document.userForm.city.value == "")
-    {
-        alert("Please provide your city");
-        document.userForm.city.focus();
-        return false;
-    }    
+    // if (document.userForm.city.value == "")
+    // {
+    //     alert("Please provide your city");
+    //     document.userForm.city.focus();
+    //     return false;
+    // }    
 
-    if ( document.myForm.zip.value == "" || 
-        isNaN( document.myForm.zip.value ) ||
-        document.myForm.zip.value.length != 5 ) 
-    {
-        alert( "Please provide a zip in the format #####." );
-        document.myForm.zip.focus() ;
-        return false;
-    }
-    return (true);
+    // if ( document.myForm.zip.value == "" || 
+    //     isNaN( document.myForm.zip.value ) ||
+    //     document.myForm.zip.value.length != 5 ) 
+    // {
+    //     alert( "Please provide a zip in the format #####." );
+    //     document.myForm.zip.focus() ;
+    //     return false;
+    // }
+    // return (true);
 
 
 }
